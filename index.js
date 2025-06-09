618ea073e69bd78f6d88ed20d11f0bc0c6e207fa
defeger <defflox@gmail.com>
2025-06-10 01:33:23 +0400

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 //middlewares
