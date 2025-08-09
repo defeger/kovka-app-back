7d6ec1852faaf398bc7eb462333c3a9659722e39
defeger <97786925+defeger@users.noreply.github.com>
2025-08-09 16:18:54 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 //middlewares
 app.use(express.json());
 app.use(cors({
-  origin: ["https://defeger-kovka-app-front-7ddb.twc1.net/"], // разрешить фронт на Vite
+  origin: ["defeger-kovka-app-front-7ddb.twc1.net"], // разрешить фронт на Vite
   credentials: true
 }));
 
